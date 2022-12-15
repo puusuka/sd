c2112eb51e13788c2ffae12e97185df8e110cd3e
puusuka <puusuka1208@icloud.com>
2022-12-16 06:39:52 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,3 @@ Route::get('/',function(){
   return view ('welcome');
 });
 Route::get('hello','App\Http\Controllers\HelloController@index');
-Route::get('hello/other','App\Http\Controllers\HelloController@other');
