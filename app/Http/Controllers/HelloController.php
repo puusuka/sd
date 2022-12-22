ac57014e3b39a0b7e0650cfb7566157348b53a18
puusuka <puusuka1208@icloud.com>
2022-12-22 12:52:52 +0900

    first commit

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,31 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
- class HelloController extends Controller{
-    public function index(Request $request,Response $response){
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello/index</title>
-    <style>
-    body {font-size:16pt; color:#999;}
-    h1 {font-size:120pt; 
-        text-align:right; color:#fafafa;
-    margin:-50px 0px -120px 0px;}
-    </style>
-    </head>
-    <body>
-    <h1>Hello</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-    </body>
-    </html>
-    EOF;
-         $response->setContent($html);
-         return $response;
-    }
- }
+ class HelloController extends Controller
+ {
+   public function index()
+{
+   $data = ['one', 'two', 'three', 'four', 'five'];
+   return view('hello.index', ['data'=>$data]);
+}
+
+  
+}
 
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@ Route::get('/',function(){
   return view ('welcome');
 });
 Route::get('hello','App\Http\Controllers\HelloController@index');
+Route::post('hello','App\Http\Controllers\HelloController@post');
